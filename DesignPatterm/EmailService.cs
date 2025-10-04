e515760470820cc523d4620053ef51404b3fb26d
Sergio Di Fiore <difiores@outlook.com>
2025-10-04 15:03:53 -0300

    Abstraction

---
+++ DesignPatterm/EmailService.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPatterm
+{
+	internal class EmailService
+	{
+		public void SendEmail()
+		{
+			Connect();
+			Authenticate();
+			Console.WriteLine($"Sending Email ...");
+			Disconnect();
+		}
+
+		private void Connect()
+		{
+			Console.WriteLine("Connecting to email server...");
+		}
+
+		private void Authenticate()
+		{
+			Console.WriteLine("Authenticating to email server...");
+		}
+
+		private void Disconnect()
+		{
+			Console.WriteLine("Disconnecting to email server...");
+		}
+	}
+}
--- DesignPatterm/Program.cs
+++ DesignPatterm/Program.cs
@@ -1,16 +1,20 @@
 ﻿using DesignPatterm;
 
-//// Encapsulation
+// Encapsulation
 //BadBankAccount badBankAccoount = new BadBankAccount();
 //badBankAccoount.Balance = -50;
 
 //Console.WriteLine(badBankAccoount.Balance);
 
-BankAccount bankAccount = new BankAccount(100);
-Console.WriteLine(bankAccount.GetBalance());
+//BankAccount bankAccount = new BankAccount(100);
+//Console.WriteLine(bankAccount.GetBalance());
 
-bankAccount.Deposit(50);
-Console.WriteLine(bankAccount.GetBalance());
+//bankAccount.Deposit(50);
+//Console.WriteLine(bankAccount.GetBalance());
 
-bankAccount.Withdraw(30);
-Console.WriteLine(bankAccount.GetBalance());
+//bankAccount.Withdraw(30);
+//Console.WriteLine(bankAccount.GetBalance());
+
+//Abstraction
+EmailService emailService = new EmailService();
+emailService.SendEmail();
